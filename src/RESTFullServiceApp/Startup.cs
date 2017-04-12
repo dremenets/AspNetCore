494483bb7e424c4a3777847084bea771586eb639
Дмитрий Ременец <dmremenec@gmail.com>
2017-04-12 19:11:51 +0300

    Register repository via Autofac

--- src/RESTFullServiceApp/Startup.cs
+++ src/RESTFullServiceApp/Startup.cs
@@ -1,17 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
+using DAL.EF6;
+using DAL.EF6.Models;
+using DAL.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace RESTFullService
+namespace RESTFullServiceApp
 {
     public class Startup
     {
